39a5a17002bc07d624ccbaea0059940dce2d44e1
Jomar Tigcal <jomar@tigcal.com>
2023-11-30 23:09:25 -0800

    Adding solution for Advent of Code 2023 Day 01

---
+++ advent-of-code-2023/README.md
@@ -0,0 +1,7 @@
+# advent-of-code-2023
+
+Solutions for [Advent of Code 2023](https://adventofcode.com/2023)
+
+| Day | Puzzle                                             | Solution                             |
+|-----|----------------------------------------------------|--------------------------------------|
+| 01  | [Trebuchet?!](https://adventofcode.com/2023/day/1) | [Solution](src/main/kotlin/Day01.kt) |
---
+++ advent-of-code-2023/src/main/kotlin/Day01.kt
@@ -0,0 +1,52 @@
+// Day 1: Trebuchet?!
+// https://adventofcode.com/2023/day/1
+
+import java.io.File
+
+private val digits = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+fun main() {
+    val lines = File("src/main/resources/Day01.txt").readLines()
+    val sum1 = lines.sumOf { line ->
+        line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+
+    }
+    println(sum1)
+
+    val sum2 = lines.sumOf { line ->
+        getFirstDigit(line) * 10 + getLastDigit(line)
+    }
+    println(sum2)
+}
+
+private fun getFirstDigit(line: String): Int {
+    var temp = line
+    line.toList().map { it.toString() }.runningReduce { acc, string -> acc + string }.forEach {
+        val stringToReplace = stringToReplace(it)
+        if (stringToReplace.isNotEmpty()) {
+            temp = temp.replace(stringToReplace, digits.indexOf(stringToReplace).toString())
+        }
+    }
+    return temp.first { it.isDigit() }.digitToInt()
+}
+
+private fun getLastDigit(line: String): Int {
+    var temp = line
+    line.reversed().toList().map { it.toString() }.runningReduce { acc, string -> acc + string }.forEach {
+        val stringToReplace = stringToReplace(it.reversed())
+        if (stringToReplace.isNotEmpty()) {
+            temp = temp.replace(stringToReplace, digits.indexOf(stringToReplace).toString())
+        }
+    }
+    return temp.last { it.isDigit() }.digitToInt()
+}
+
+private fun stringToReplace(line: String): String {
+    digits.forEachIndexed { _, digit ->
+        if (line.contains(digit)) {
+            return digit
+        }
+    }
+
+    return ""
+}
