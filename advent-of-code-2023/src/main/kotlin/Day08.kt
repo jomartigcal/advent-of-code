b3664905def21f5814fe115f5d3c4c9335d870d1
Jomar Tigcal <jomar@tigcal.com>
2023-12-08 12:26:02 -0800

    Adding solution for Advent of Code 2023 Day 08 (Part 2)

--- advent-of-code-2023/src/main/kotlin/Day08.kt
+++ advent-of-code-2023/src/main/kotlin/Day08.kt
@@ -2,6 +2,7 @@
 // https://adventofcode.com/2023/day/8
 
 import java.io.File
+import kotlin.math.min
 
 fun main() {
     val lines = File("src/main/resources/Day08.txt").readLines()
@@ -9,7 +10,8 @@ fun main() {
     val steps = lines.first().toList()
     val map = lines.drop(2)
 
-    println(getHumanSteps(map, steps) + 1)
+    println(getHumanSteps(map, steps))
+    println(getGhostSteps(map, steps))
 }
 
 private fun getHumanSteps(map: List<String>, steps: List<Char>): Int {
@@ -22,12 +24,52 @@ private fun getHumanSteps(map: List<String>, steps: List<Char>): Int {
         counter++
 
         val move = steps[counter % steps.size]
-        val step = if (move == 'L') {
-            map[position].substringAfter("(").substringBefore(", ")
-        } else {
-            map[position].substringAfter(", ").substringBefore(")")
-        }
+        val step = getNextPath(map[position], move)
         position = map.indexOfFirst { it.startsWith(step) }
     }
-    return counter
+    return counter + 1
+}
+
+private fun getGhostSteps(map: List<String>, steps: List<Char>): Long {
+    val starts = map.filter { it[2] == 'A' }
+
+    val counts = starts.map { start ->
+        var counter = 0L
+        var index = 0
+        var step = start
+
+        while (step[2] != 'Z') {
+            val move = steps[index]
+            val next = getNextPath(step, move)
+            step = map.first { it.startsWith(next) }
+
+            index = (index + 1) % steps.size
+            counter++
+        }
+        counter
+    }
+
+    return counts.reduce { acc, i ->
+        lcm(acc, i)
+    }
+}
+
+private fun getNextPath(string: String, move: Char): String {
+    return if (move == 'L') {
+        string.substringAfter("(").substringBefore(", ")
+    } else {
+        string.substringAfter(", ").substringBefore(")")
+    }
+}
+
+private fun lcm(num1: Long, num2: Long): Long {
+    var lcm = min(num1, num2)
+    while (lcm <= num1 * num2) {
+        if (lcm % num1 == 0L && lcm % num2 == 0L) {
+            return lcm
+        }
+        lcm += min(num1, num2)
+    }
+
+    return lcm
 }
