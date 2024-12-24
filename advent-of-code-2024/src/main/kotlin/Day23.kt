1595ab83bd6db70dab8aae9d8c8267ed1b87853d
Jomar Tigcal <jomar@tigcal.com>
2024-12-23 17:43:16 -0800

    Adding solution for Advent of Code 2024 Day 23 (Part 2)

--- advent-of-code-2024/src/main/kotlin/Day23.kt
+++ advent-of-code-2024/src/main/kotlin/Day23.kt
@@ -4,31 +4,51 @@
 import java.io.File
 
 fun main() {
-    val connections = File("src/main/resources/Day23.txt").readLines()
-
     val map = mutableMapOf<String, HashSet<String>>()
+
+    val connections = File("src/main/resources/Day23.txt").readLines()
     connections.forEach {
         val (x, y) = it.split("-")
-        map.computeIfAbsent(x, { hashSetOf() })
+        map.getOrPut(x) { hashSetOf() }
             .add(y)
-        map.computeIfAbsent(y, { hashSetOf() })
+        map.getOrPut(y) { hashSetOf() }
             .add(x)
     }
 
-    val chiefConnections = map.keys.filter { it.startsWith("t") }.associateWith { map[it] }
-
     val pairs = mutableListOf<Pair<String, String>>()
-    chiefConnections.forEach { con ->
-        con.value?.forEach {
+    map.forEach { con ->
+        con.value.forEach {
             pairs.add(con.key to it)
         }
     }
 
-    pairs.map { pair ->
-        val x = map.getOrDefault(pair.first, emptyList())
-        val y = map.getOrDefault(pair.second, emptyList())
-        (x intersect y).map {
-            Triple(pair.first, pair.second, it)
+    pairs
+        .filter { it.first.startsWith("t") || it.second.startsWith("t") }
+        .map { pair ->
+            val x = map.getOrDefault(pair.first, emptyList()).toSet()
+            val y = map.getOrDefault(pair.second, emptyList()).toSet()
+            (x intersect y).map {
+                Triple(pair.first, pair.second, it)
+            }
         }
-    }.flatten().distinctBy { it.toList().sorted() }.also { println(it.size) }
+        .flatten()
+        .distinctBy { it.toList().sorted() }
+        .also { println(it.size) }
+
+    pairs
+        .map { pair ->
+            val x = map.getOrDefault(pair.first, emptyList()).toSet()
+            val y = map.getOrDefault(pair.second, emptyList()).toSet()
+            buildList {
+                add(pair.first)
+                add(pair.second)
+                addAll(x intersect y)
+            }.sorted()
+        }
+        .groupingBy { it }
+        .eachCount()
+        .maxBy { it.value }.key
+        .also {
+            println(it.joinToString(","))
+        }
 }
