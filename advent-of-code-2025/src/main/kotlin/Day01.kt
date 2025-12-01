b47f9f6f66a9b97742fd874b3b624896282b6cc3
Jomar Tigcal <jomar@tigcal.com>
2025-12-01 15:47:14 -0800

    Adding solution for Advent of Code 2025 Day 01 (Part 2)

--- advent-of-code-2025/src/main/kotlin/Day01.kt
+++ advent-of-code-2025/src/main/kotlin/Day01.kt
@@ -5,6 +5,7 @@ import kotlin.math.abs
 // https://adventofcode.com/2025/day/1
 
 private const val COUNT = 100
+private const val START_INDEX = 50
 
 fun main() {
     val rotations = File("src/main/resources/Day01.txt").readLines().map {
@@ -14,12 +15,22 @@ fun main() {
     }
 
     turnDial(rotations)
+    countZeroClicks(rotations)
 }
 
 private fun turnDial(rotations: List<Pair<Char, Int>>) {
-    var index = 50
-    rotations.map {
-        index = turnDial(value = index, count = it.second, left = it.first == 'L')
+    var index = START_INDEX
+    rotations.map { rotation ->
+        index = if (isLeftTurn(rotation)) {
+            val diff = (index - rotation.second) % COUNT
+            if (diff < 0) {
+                abs(COUNT + diff)
+            } else {
+                diff
+            }
+        } else {
+            (index + rotation.second) % COUNT
+        }
         index
     }.count {
         it == 0
@@ -28,15 +39,29 @@ private fun turnDial(rotations: List<Pair<Char, Int>>) {
     }
 }
 
-private fun turnDial(value: Int, count: Int, left: Boolean): Int {
-    return if (left) {
-        val diff = (value - count) % COUNT
-        if (diff < 0) {
-            abs(COUNT + diff)
+private fun countZeroClicks(rotations: List<Pair<Char, Int>>) {
+    var index = START_INDEX
+    rotations.map { rotation ->
+        var zeroClicks: Int
+        if (isLeftTurn(rotation)) {
+            zeroClicks = (rotation.second - index) / COUNT
+            if (rotation.second >= index && index != 0) zeroClicks++
+
+            val diff = (index - rotation.second) % COUNT
+            index = if (diff < 0) {
+                abs(COUNT + diff)
+            } else {
+                diff
+            }
         } else {
-            diff
+            val oldIndex = index
+            index = (oldIndex + rotation.second) % COUNT
+            zeroClicks = (oldIndex + rotation.second) / COUNT
         }
-    } else {
-        (value + count) % COUNT
-    }
+        zeroClicks
+    }.sumOf {
+        it
+    }.also { println(it) }
 }
+
+private fun isLeftTurn(rotation: Pair<Char, Int>): Boolean = rotation.first == 'L'
